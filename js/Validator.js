ccb324dba6f8609e7ab15b6e220b0109846ec27f
ssslavev <s.s.slavev@hotmail.com>
2019-04-01 12:46:18 +0300

    made Validation and layout improvements

--- js/Validator.js
+++ js/Validator.js
@@ -1,4 +1,5 @@
 const EMAIL_REGEX = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+const PHONE_NUMBER = /^0\d{4,5}$/;
 
 export default class Validator {
 
@@ -10,12 +11,15 @@ export default class Validator {
             if (fullName === '') {
                 $('.toast .toast-body').html("Full name can not be empty!")
                 $('.toast').toast('show');
+                throw('Name is empty!');
             } else if (fullName.length < 3 || fullName.length > 20) {
                 $('.toast .toast-body').html("Username must be between 3 and 20 characters!")
                 $('.toast').toast('show');
+                throw('Username must be between 3 and 20 characters!');
             } else if (!(fullName.trim().indexOf(' ') != -1)) {
                 $('.toast .toast-body').html("The names must be two!")
                 $('.toast').toast('show');
+                throw('The names must be two!');
             }
         });
 
@@ -26,9 +30,64 @@ export default class Validator {
         if (email === '') {
             $('.toast .toast-body').html("Email is required!")
             $('.toast').toast('show');
+            throw('Email is required!');
         } else if  (!email.match(EMAIL_REGEX)) {
             $('.toast .toast-body').html("Email is not valid!")
             $('.toast').toast('show');
+            throw('Email is not valid!');
+        }
+    }
+
+    static validatePhoneCode(phoneCode) {
+        if(phoneCode==='') {
+            $('.toast .toast-body').html("Phone code is required!")
+            $('.toast').toast('show');
+            throw('Phone code is required!');
+            
+        } else {
+            phoneCode = phoneCode.trim();
+            
+        }
+    }
+
+    static validatePhoneNumber(phoneNumber) {
+        if(phoneNumber==='') {
+            $('.toast .toast-body').html("Phone number is required!")
+            $('.toast').toast('show');
+            throw('Phone number is required!');
+        } else if(isNaN(phoneNumber)) {
+            $('.toast .toast-body').html("Phone number can include just digits!")
+            $('.toast').toast('show');
+            throw('Phone number can include just digits');
+        } else if(phoneNumber[0]!=0) {
+            $('.toast .toast-body').html("Firs digit must be 0!")
+            $('.toast').toast('show');
+            throw('Firs digit must be 0!');
+        } else if (phoneNumber.length<5) {
+            $('.toast .toast-body').html("Phone number must be minimum 5 digits!")
+            $('.toast').toast('show');
+            throw('Phone number must be minimum 5 digits!');
+        }
+
+        
+
+
+    }
+
+    static validateCountry(country) {
+            if(country==='') {
+                $('.toast .toast-body').html("Country is required!")
+                $('.toast').toast('show');
+                throw('Country is required!');
+            }
+
+    }
+
+    static validateCheckbox(value) {
+        if(!value) {
+            $('.toast .toast-body').html("You have to accept Terms and Conditions!")
+            $('.toast').toast('show');
+            throw('You have to accept Terms and Conditions!');
         }
     }
 
--- js/main.js
+++ js/main.js
@@ -3,6 +3,21 @@ import Validator from "./Validator.js";
 $(function () {
 
     let submit = document.getElementById("send-button");
+    let select = document.getElementById("select");
+    let country;
+    
+    
+
+    select.addEventListener('change', function() {
+        country = document.getElementById("select").value;
+        if(country === "BG") {
+            $('#phone-code').val("+359");
+        } else if(country === "US") {
+            $('#phone-code').val("+1");
+        } else if(country=== "GB") {
+            $('#phone-code').val("+44");
+        }
+    })
 
 
     submit.addEventListener('click', function () {
@@ -11,10 +26,19 @@ $(function () {
         let email = document.getElementById("email").value;
         let phoneCode = document.getElementById("phone-code").value;
         let phoneNumber = document.getElementById("phone-number").value;
-        let country = document.getElementById("select").value;
+        let checkbox = document.getElementById("checkbox").checked;
 
         Validator.validateName(fullName);
         Validator.validateEmail(email);
+        Validator.validatePhoneNumber(phoneNumber);
+        Validator.validateCountry(country);
+        Validator.validateCheckbox(checkbox);
+        Validator.validatePhoneCode(phoneCode);
+
+        $('.toast .toast-body').html("Everything is correct!")
+            $('.toast').toast('show');
+
+
 
         
     })
